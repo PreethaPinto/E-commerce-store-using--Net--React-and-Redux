001cbd9e5d28dccbbdae55ff02681496beff010c
PreethaPinto <preethacp@gmail.com>
2023-11-13 23:25:30 +1000

    Add Catalog component

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,22 @@
+import { Product } from '../../app/models/products';
+
+interface Props {
+  products: Product[];
+  addProduct: () => void;
+}
+
+const Catalog = ({ products, addProduct }: Props) => {
+  return (
+    <>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
+    </>
+  );
+};
+export default Catalog;
